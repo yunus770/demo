c84e28af29b1c70ff9a2db2280fb8e6e833a5c0e
Agoyli <agoyliq@gmail.com>
2024-12-14 16:38:47 +0500

    first_commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./**/*.html"],
+  darkMode:'class',
+  theme: {
+    extend: {
+    colors:{
+      mainColor: '#f1f5f9'
+    },
+    padding:{
+      big:'50px'
+    },
+    screens: {
+      'sm':'640px',
+      'md':'768px',
+      'lg':'1024px',
+      'xl':'1280px',
+      
+
+    }
+    },
+  },
+  plugins: [],
+}
+
